20b19f902b3be887fe1b4548d9c6696aa86a3015
git-bhaskar <bhaskar.nato@gmail.com>
2025-05-30 13:57:04 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
 function OpenLink(element, url) {
             window.open(url, '_blank');
         }
-        
+
+function showSeason(season) {
+            const seasons = document.querySelectorAll('.episode-list');
+            seasons.forEach(s => s.style.display = 'none');
+            document.getElementById(`season-${season}`).style.display = 'block';
+
+            const buttons = document.querySelectorAll('.season-tab');
+            buttons.forEach(b => b.classList.remove('active'));
+            buttons[season - 1].classList.add('active');
+        }
+function OpenLink(element, url) {
+            window.open(url, '_blank');
+}
